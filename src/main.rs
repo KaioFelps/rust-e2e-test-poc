3b4066d83d33230420c0067426dae798843ca25c
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-04-18 22:54:02 -0300

    build: setup `main` function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web::Data, HttpServer};
+use config::{
+    datastore::get_datastore, inertia::get_inertia, options::Options, server::get_server,
+    vite::get_vite,
+};
+
+mod config;
+mod domain;
+mod infra;
+
+#[actix_web::main]
+async fn main() -> anyhow::Result<()> {
+    let options = Options::get();
+
+    let vite = get_vite().await?;
+    let inertia = Data::new(get_inertia(vite).await?);
+    let datastore = Data::new(get_datastore(options.get_main_database_url()).await?);
+
+    if let Err(err) = sqlx::migrate!().run(datastore.get_db()).await {
+        log::error!("{err}");
+    }
+
+    Ok(HttpServer::new(move || {
+        get_server()
+            .app_data(datastore.clone())
+            .app_data(inertia.clone())
+    })
+    .bind(("127.0.0.1", 3000))?
+    .run()
+    .await?)
 }
