5cbd252b07d90adc6f51954b3d54ef48a6e4048c
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-04-20 03:14:41 -0300

    test: add common module's `mod.rs` file to expose it as a test module

---
+++ tests/common/mod.rs
@@ -0,0 +1,9 @@
+pub mod datastore_guard;
+pub mod setup;
+pub mod testing_app;
+
+pub fn get_unique_db_schema() -> String {
+    use std::sync::atomic::{AtomicUsize, Ordering};
+    static ID: AtomicUsize = AtomicUsize::new(0);
+    format!("test_schema_{}", ID.fetch_add(1, Ordering::SeqCst))
+}
