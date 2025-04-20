03d76c2d96e2d6d75e5ddd85d0c32c9aac83a2b3
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-04-20 03:00:02 -0300

    change(vite): set `ViteMode` to `Manifest` when RUST_ENV variable is not "development"

--- src/config/vite.rs
+++ src/config/vite.rs
@@ -1,10 +1,16 @@
 use vite_rust::{Vite, ViteConfig};
 
+use super::options::{Options, RustEnv};
+
 pub async fn get_vite() -> anyhow::Result<Vite> {
-    let vite_config = ViteConfig::default()
+    let mut vite_config = ViteConfig::default()
         .set_manifest_path("public/bundle/manifest.json")
         .set_entrypoints(vec!["www/app.tsx"])
         .set_prefix("bundle");
+
+    if *Options::get().get_environment() != RustEnv::Development {
+        vite_config = vite_config.set_force_mode(vite_rust::ViteMode::Manifest);
+    }
 
     Ok(Vite::new(vite_config).await?)
 }
