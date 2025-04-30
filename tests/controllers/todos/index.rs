b64c7da00a9b493c3fbe748c6607e0cefaf02d1f
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-04-30 00:41:49 -0300

    test(TodosController::index): it should get to filter todos by `completed` status

--- tests/controllers/todos/index.rs
+++ tests/controllers/todos/index.rs
@@ -4,7 +4,11 @@ use crate::common::{
     setup::__setup,
 };
 use actix_web::{http::StatusCode, test::TestRequest, web::Data};
-use e2e_test_poc::config::{datastore::DataStore, server::get_server};
+use chrono::{Duration, Utc};
+use e2e_test_poc::{
+    config::{datastore::DataStore, server::get_server},
+    infra::http::presenters::todo_presenter::TodoPresenter,
+};
 use inertia_rust::{
     test::{InertiaTestRequest, IntoAssertableInertia},
     Inertia,
@@ -13,16 +17,25 @@ use pretty_assertions::assert_eq;
 use rstest::rstest;
 
 async fn seed_todos(datastore: &DataStore) {
+    let created_at = Utc::now();
+    let created_at_2 = created_at + Duration::minutes(10);
+    let created_at_3 = created_at_2 + Duration::hours(2);
+
     let has_inserted = sqlx::query(
-        r#"INSERT INTO "todos"
-        (id, title, content, completed)
-        VALUES
-        (1, 'Foo', 'Content of foo', false),
-        (2, 'Bar', 'Content of bar', true),
-        (3, 'Baz', 'Content of baz', true)"#,
+        r#"INSERT INTO "todos" (id, title, content, completed, created_at)
+        VALUES  (1, 'Foo', 'Content of foo', false, $1::TIMESTAMP),
+                (2, 'Bar', 'Content of bar', true, $2::TIMESTAMP),
+                (3, 'Baz', 'Content of baz', true, $3::TIMESTAMP)"#,
     )
+    .bind(created_at)
+    .bind(created_at_2)
+    .bind(created_at_3)
     .execute(datastore.get_db())
     .await;
+
+    if let Err(ref err) = has_inserted {
+        log::error!("{err}");
+    }
 
     assert!(has_inserted.is_ok());
 }
@@ -70,7 +83,7 @@ async fn can_fetch_latest_todos(
     assert_eq!(3, page.props["todos"]["data"].as_array().unwrap().len());
     assert_eq!(3, page.props["todos"]["pagination"]["totalItems"]);
     assert_eq!(
-        "Foo",
+        "Baz",
         page.props["todos"]["data"][0]["title"].as_str().unwrap()
     );
 }
@@ -127,4 +140,50 @@ async fn can_fetch_todos_filtered_by_content(
     assert_eq!("Baz", todo["title"].as_str().unwrap());
     assert_eq!("Content of baz", todo["content"].as_str().unwrap());
     assert_eq!(true, todo["completed"]);
+}
+
+#[rstest]
+#[awt]
+#[tokio::test]
+async fn can_fetch_todos_filtered_by_completed_status(
+    #[future] __setup: (),
+    #[future] inertia: Data<Inertia>,
+    #[future] mut datastore: DataStoreGuard<'_>,
+) {
+    seed_todos(datastore.as_ref()).await;
+
+    let app =
+        actix_web::test::init_service(get_server().app_data(inertia).app_data(datastore.take()))
+            .await;
+
+    let response = TestRequest::get()
+        .inertia()
+        .uri("/?query_by=completed&query=true")
+        .send_request(&app)
+        .await;
+
+    assert!(response.status().is_success());
+
+    let page = response.into_assertable_inertia();
+
+    assert_eq!(2, page.props["todos"]["data"].as_array().unwrap().len());
+    assert_eq!(2, page.props["todos"]["pagination"]["totalItems"]);
+
+    let first_todo =
+        serde_json::from_value::<TodoPresenter>(page.props["todos"]["data"][0].clone()).unwrap();
+
+    let second_todo =
+        serde_json::from_value::<TodoPresenter>(page.props["todos"]["data"][1].clone()).unwrap();
+
+    assert!(first_todo.created_at >= second_todo.created_at);
+
+    assert_eq!(3, first_todo.id);
+    assert_eq!("Baz", first_todo.title);
+    assert_eq!("Content of baz", first_todo.content);
+    assert_eq!(true, first_todo.completed);
+
+    assert_eq!(2, second_todo.id);
+    assert_eq!("Bar", second_todo.title);
+    assert_eq!("Content of bar", second_todo.content);
+    assert_eq!(true, second_todo.completed);
 }
