ff1e14f2d60d1e8e1779255462530dd6a358e3e3
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-04-30 02:28:13 -0300

    chore: config typescript aliases with vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,29 @@ import laravel from 'laravel-vite-plugin';
 import tailwind from "@tailwindcss/vite"
 import type { UserConfig } from 'vite';
 
+import tsconfig from "./tsconfig.json";
+import path from 'path';
+
+export const tsconfigPathAliases = Object.fromEntries(
+  Object.entries(tsconfig.compilerOptions.paths).map(([key, values]) => {
+    let value = values[0];
+    if (key.endsWith("/*")) {
+      key = key.slice(0, -2);
+      value = value.slice(0, -2);
+    }
+
+    const nodeModulesPrefix = "node_modules/";
+    if (value.startsWith(nodeModulesPrefix)) {
+      value = value.replace(nodeModulesPrefix, "");
+    } else {
+      value = path.join(__dirname, value);
+    }
+
+    return [key, value];
+  }),
+);
+
+
 export default {
   plugins: [    
       tailwind(),
@@ -19,5 +42,8 @@ export default {
       watch: {
           ignored: ["*"]
       }
-  }
+  },
+  resolve: {
+    alias: tsconfigPathAliases,
+  },
 } satisfies UserConfig;
