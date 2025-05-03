8d19cbe6fd9acf40e5182944ac6c98041d557eb2
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-05-03 01:27:39 -0300

    test(TodosController::create): it should validate request body

--- tests/controllers/todos/create.rs
+++ tests/controllers/todos/create.rs
@@ -1,6 +1,9 @@
 use actix_web::{http::StatusCode, test::TestRequest, web::Data};
 use e2e_test_poc::config::server::get_server;
-use inertia_rust::Inertia;
+use inertia_rust::{
+    test::{InertiaTestRequest, IntoAssertableInertia},
+    Inertia,
+};
 use pretty_assertions::assert_eq;
 use rstest::rstest;
 use serde_json::json;
@@ -10,13 +13,14 @@ use crate::common::{
         datastore::{datastore, DataStoreGuard},
         inertia::inertia,
     },
+    helpers::{extract_location_from_headers, extract_session_cookie},
     setup::__setup,
 };
 
 #[rstest]
 #[awt]
 #[tokio::test]
-async fn create(
+async fn it_should_create_a_valid_todo(
     #[future] __setup: (),
     #[future] inertia: Data<Inertia>,
     #[future] mut datastore: DataStoreGuard<'_>,
@@ -55,4 +59,39 @@ async fn create(
         "Expected it to always redirect to '/', regardless it's succeeded."
     );
     assert_eq!(1, count);
+}
+
+#[rstest]
+#[awt]
+#[tokio::test]
+async fn it_should_validate_request_body(
+    #[future] __setup: (),
+    #[future] inertia: Data<Inertia>,
+    #[future] mut datastore: DataStoreGuard<'_>,
+) {
+    let app =
+        actix_web::test::init_service(get_server().app_data(inertia).app_data(datastore.take()))
+            .await;
+
+    let response = TestRequest::post()
+        .uri("/create")
+        .set_json(json!({ "title": "task" }))
+        .inertia()
+        .send_request(&app)
+        .await;
+
+    assert_eq!(StatusCode::FOUND, response.status());
+
+    let subsequent_response = TestRequest::get()
+        .uri(extract_location_from_headers(&response))
+        .inertia()
+        .cookie(extract_session_cookie(&response))
+        .send_request(&app)
+        .await;
+
+    let page = subsequent_response.into_assertable_inertia();
+    println!("{page:#?}");
+
+    assert!(page.get_props()["errors"]["title"].is_string());
+    assert!(page.get_props()["errors"]["content"].is_string());
 }
