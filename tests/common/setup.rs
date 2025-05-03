e6eabbebce12b66ddb5e31599d66e7f4378f5509
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-05-03 01:22:18 -0300

    test(setup): load `.env.test` variables rather than `.env`

--- tests/common/setup.rs
+++ tests/common/setup.rs
@@ -1,11 +1,11 @@
-use dotenvy::dotenv;
 use e2e_test_poc::config::options::Options;
 use env_logger::Target;
 use rstest::fixture;
 
 #[fixture]
 pub async fn __setup() {
-    dotenv().expect("Failed to inject `.env` into environment variables.");
+    dotenvy::from_filename(".env.test")
+        .expect("Failed to inject `.env` into environment variables.");
 
     std::env::set_var("RUST_ENV", "test");
 
